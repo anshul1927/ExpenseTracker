64131b1c576a974ca4a83cbdfe1f67a5ea6204a1
anshullkgarg <anshul.garg@payufin.com>
2022-02-16 18:26:59 +0530

    user model class

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,6 +82,7 @@ DATABASES = {
     }
 }
 
+# AUTH_USER_MODEL = "users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
