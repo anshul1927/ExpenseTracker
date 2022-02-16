0bb263183666351d0b3230ffb94f74e62ba409d4
anshullkgarg <anshul.garg@payufin.com>
2022-02-16 18:30:54 +0530

    user model class

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+
+class User(models.Model):
+    """Custom User Model"""
+    first_name = models.CharField(_("first name"), max_length=30, blank=True, default="First Name")
+    last_name = models.CharField(_("Last name"), max_length=30, blank=True, default="Last Name")
+    email = models.EmailField(_("Email ID"), blank=True, default="Email ID")
+    password = models.CharField(_("Password"),max_length=40, blank=True, default="Password")
