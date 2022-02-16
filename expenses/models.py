921697858d4714021c921c464b8a5c8a890efcb2
utkarsh-008 <utkarsh.rai@payufin.com>
2022-02-16 20:29:53 +0530

    Group and Expense model

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from groups.models import Group
+from users.models import User
+
+
+class Expense(models.Model):
+    expense_name = models.CharField(max_length=255)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    payee_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
+
+
+class ExpenseToUser(models.Model):
+    expense_id = models.ForeignKey(Expense, on_delete=models.CASCADE)
+    users_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount_paid = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import User
+
 
 # Create your models here.
+class Group(models.Model):
+    GROUP_HOME = 'H'
+    GROUP_TRIP = 'T'
+    GROUP_OTHER = 'O'
+
+    GROUP_TYPE_CHOICES =[
+        (GROUP_HOME, 'HOME'),
+        (GROUP_TRIP, 'TRIP'),
+        (GROUP_OTHER, 'OTHER'),
+    ]
+    name = models.CharField(max_length=1024)
+    group_type = models.CharField(max_length=1, choices=GROUP_TYPE_CHOICES, default=GROUP_HOME)
+    description = models.TextField()
+    created_at = models.DateField(auto_now=True)
+    created_by = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class GroupToUser(models.Model):
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = {'group_id', 'user_id'}
